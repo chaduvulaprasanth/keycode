9b35b798dd14063a2c4083c08524f3576b55110a
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2019-08-31 21:28:26 +0530

    updated json file in  keycode

--- main.js
+++ main.js
@@ -3,14 +3,9 @@ function getcode(event){
 	document.getElementById("keycode").innerText = event.keyCode;
 	if(event.keyCode == 32){
 	document.getElementById("keyname").innerText = "Enter key is Space";
-
 	}
 	else{
 	document.getElementById("keyname").innerText = `Enter key is ${event.key}`;
-
 	}
-
-	console.log(event.keyCode);
-	console.log(event.key);
 }
-document.body.addEventListener("keydown",getcode)
+window.addEventListener("keydown",getcode)
